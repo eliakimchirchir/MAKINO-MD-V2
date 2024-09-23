7b2ccbf636929e0be6026a43a68c8fab317416aa
eliakimchirchir <eliakimkiplagat12@yahoo.com>
2024-09-23 23:41:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -19,10 +19,10 @@ global.ANTI_BOT = false;
 global.PM_BLOCKER = false;
 global.CHATBOT = false;
 
-global.SESSION_ID = process.env.SESSION_ID || "taira-tech-a9929c12cfccdf1476d2aa5e12def296"
-global.Owner = ["2348138686232"]; //like 2347080968564 
-global.OwnerName =  "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";
-global.BotName = "♱MAKINO-MD-V2♱♡⃤";
+global.SESSION_ID = process.env.SESSION_ID || "{"noiseKey":{"private":{"type":"Buffer","data":"YFrQLrUFMk2/RZYILOyXx6oU9KXHZkkPy7WpPHw/cVg="},"public":{"type":"Buffer","data":"OmZux/aGV6EZDr9R6pfIDp1VxqjPsYMn3U3UoCsYZQ8="}},"pairingEphemeralKeyPair":{"private":{"type":"Buffer","data":"GGOWQO/Fx5h/Kn+jlSRevI1/7u0hxLGWROYgfOxpBl4="},"public":{"type":"Buffer","data":"PikEc0yD/JQfnBveWUscj5s+xHT6XHdDEnuyS0RYL2E="}},"signedIdentityKey":{"private":{"type":"Buffer","data":"+ICV7GArJSA+Djsp9iz0L8KY3vzJ+hPMgP3zt/PRvn4="},"public":{"type":"Buffer","data":"yfNOO8iWK+iBMWAyWgVARLJiJXtIrwAdhOMS9pL3zA0="}},"signedPreKey":{"keyPair":{"private":{"type":"Buffer","data":"UNF5NCF/J9QAIxCrmcX23NognvmSIH+tJctzKtAvu1o="},"public":{"type":"Buffer","data":"yEud3pbzsO0l1OP6EehSoKCuhLjjV7kItzvjbPmV1Bc="}},"signature":{"type":"Buffer","data":"Xza966FKTj4sjgSmzUHzbnm6UG5c9bwvcqd2ymoMes/ithFTWtFhMTgWkLXfTEeIgUQseLx5jn33Y2aHBkRigw=="},"keyId":1},"registrationId":38,"advSecretKey":"tjf/Kg0+ZAcxXLuO/AWkmVDHy1zhwzjH3MRBNb2o9L0=","processedHistoryMessages":[],"nextPreKeyId":31,"firstUnuploadedPreKeyId":31,"accountSyncCounter":0,"accountSettings":{"unarchiveChats":false},"deviceId":"n3oK0IgKTZSpGB6ra-F26w","phoneId":"f28471bc-5f62-4528-a492-a36bd4e6871c","identityId":{"type":"Buffer","data":"vB31qWCBESsnfI33jDy+c5zAwzU="},"registered":true,"backupToken":{"type":"Buffer","data":"mA2fp+FW9Dz9WFwP9izo5D5cTzg="},"registration":{},"pairingCode":"9MRQJYLH","me":{"id":"254728705726:70@s.whatsapp.net"},"account":{"details":"CNDr+7kBEJ2ax7cGGAQgACgA","accountSignatureKey":"ydeN+1alw3gZH9FPP1fZbEQv6N4Tn8U4+CGao9nm5gM=","accountSignature":"x2I2g6dS6Qpr29Z0e0hqQaNjKqudYTlWzkVK6bDplJrXRULscRzEF5gQfquQWI1cmfZS1btBMdty4gQaLyh/Dw==","deviceSignature":"NkQE7f6X4D48n9jQtLMvWeABHe2ot8lHnuwNiHmz+Qh6h7srfGXt78tni2RZZnDHHMU7mILQjoLG0Nj8yjAXgQ=="},"signalIdentities":[{"identifier":{"name":"254728705726:70@s.whatsapp.net","deviceId":0},"identifierKey":{"type":"Buffer","data":"BcnXjftWpcN4GR/RTz9X2WxEL+jeE5/FOPghmqPZ5uYD"}}],"platform":"android","lastAccountSyncTimestamp":1727122731,"myAppStateKeyId":"AAAAALkx"}"
+global.Owner = ["254728705726"]; //like 2347080968564 
+global.OwnerName =  "ELIAKIM PYTHON PROGRAMMER";
+global.BotName = "ELIAKIM PYTHON PROGRAMMER";
 global.packname = "♱MAKINO-MD-V2♱♡⃤";                             //Do not change.
 global.author = "TAIRA MAKINO";                               //Do not change.
 global.BotSourceCode = "https://github.com/anonphoenix007/MAKINO-MD-V2"; //Do not change.
